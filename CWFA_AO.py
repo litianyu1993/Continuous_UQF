Fixed the gradient descent, added directly learning WFA using GD.

CWFA_AO.py
@@ -11,8 +11,8 @@ class CWFA_AO:
         self.output_dim = Omega.shape[1] if Omega.ndim > 1 else 1
 
 
-    def update_dynamics(self, prev, action, obs):
 
+    def update_dynamics(self, prev, action, obs):
         tmp = tl.tenalg.contract(self.A, 1, action, 0)
         tmp = tl.tenalg.contract(obs, 0, tmp, 1)
         next = tl.tenalg.contract(tmp, 0, prev, 0)
@@ -24,12 +24,30 @@ class CWFA_AO:
         return term
 
     def predict(self, act_seq, obs_seq):
-        current_state = self.alpha
-        for a, o in zip(act_seq, obs_seq):
-            current_state = self.update_dynamics(current_state, a, o)
-        term = self.term_dynamics(current_state).ravel()
-        pred = term if self.output_dim > 1 else term[0]
-        return np.asarray(pred)
+        # current_state = self.alpha
+        # for a, o in zip(act_seq, obs_seq):
+        #     current_state = self.update_dynamics(current_state, a, o)
+        # term = self.term_dynamics(current_state).ravel()
+        # pred = term if self.output_dim > 1 else term[0]
+        mps = []
+        for t in range(act_seq.shape[1]):
+            if t == 0:
+                mps.append(np.einsum('ip,pjkl ->ijkl', self.alpha.reshape(1, -1), self.A))
+            elif t == act_seq.shape[1] - 1:
+                mps.append(np.einsum('pjkl, lm -> pjkm', self.A, self.Omega))
+            else:
+                mps.append(self.A)
+        contracted = []
+        for t in range(act_seq.shape[1]):
+            tmp1 = act_seq[:, t, :]
+            tmp2 = obs_seq[:, t, :]
+            #print(mps[t].shape, tmp1.shape, tmp2.shape)
+            temp_core = np.einsum('pjkl, ij, ik->pil', mps[t], tmp1, tmp2)
+            contracted.append(temp_core)
+        tmp_contract = contracted[0]
+        for t in range(1, len(contracted)):
+            tmp_contract = np.einsum('pil, lik->pik', tmp_contract, contracted[t])
+        return np.asarray(tmp_contract).squeeze()
 
 
     def build_true_Hankel_tensor(self,l):
@@ -38,6 +56,7 @@ class CWFA_AO:
             H = np.tensordot(H,self.A,[H.ndim-1,0])
         H = np.tensordot(H,self.Omega,[H.ndim-1,0])
         return H
+
 
 
 if __name__ == '__main__':

Continuous_Hankel_GD.py
@@ -91,7 +91,7 @@ def init_weight(w):
 
 class Hankel(nn.Module):
     def __init__(self, action_dim, obs_dim, rank, encoded_dim_action, encoded_dim_obs, hidden_units_action, hidden_units_obs
-                 , output_dim, max_length, seed = 0, device = 'cpu', rescale = True, freeze_encoder = False,
+                 , output_dim, max_length, seed = 0, device = 'cpu', rescale = False, freeze_encoder = False,
                  encoder_action = None, encoder_obs = None):
         super(Hankel, self).__init__()
         np.random.seed(seed)
@@ -123,15 +123,16 @@ class Hankel(nn.Module):
             if k == max_length - 1:
                 dim_3 = output_dim
 
-            H_tmp = tl.tensor(np.random.rand(dim_0, dim_1, dim_2, dim_3))
+            #H_tmp = tl.tensor(np.random.rand(dim_0, dim_1, dim_2, dim_3))*2 - 1
+            H_tmp = tl.tensor(np.random.normal(0, 1, [dim_0, dim_1, dim_2, dim_3]))
             identity = np.zeros([dim_0, dim_1*dim_2, dim_3])
             for j in range(min(dim_0, dim_3)):
                 identity[j, :, j] += 1.
             if rescale:
-                H_tmp = init_weight(H_tmp)
-                #H_tmp = init_weight(H_tmp) + identity.reshape([dim_0, dim_1, dim_2, dim_3])
+                #H_tmp = init_weight(H_tmp)
+                H_tmp = init_weight(H_tmp) + identity.reshape([dim_0, dim_1, dim_2, dim_3])
 
-            H_tmp = tl.tensor(H_tmp, device=device, requires_grad=True)
+            H_tmp = torch.nn.parameter.Parameter(torch.tensor(H_tmp.float(), requires_grad=True)).to(device)
             self.H.append(H_tmp)
 
     def init_FC_encoder(self, hidden_units, input_dim, encoded_dim, freeze_encoder):
@@ -157,7 +158,7 @@ class Hankel(nn.Module):
         x = x.float()
         for i in range(len(encoder_FC)):
             x = encoder_FC[i](x)
-            x = F.relu(x)
+            x = F.tanh(x)
         return x
 
     def forward(self, action, obs):
@@ -182,7 +183,7 @@ class Hankel(nn.Module):
         #print(contracted.shape)
         return contracted
     def get_params(self):
-        params = []
+        params = nn.ParameterList([])
         for i in range(len(self.encoder_action)):
             params.append(self.encoder_action[i].weight)
             params.append(self.encoder_action[i].bias)

Get_Prob_reward.py
@@ -11,11 +11,14 @@ def sliding_window(action_obs, rewards, window_size, action_all, obs_all):
     windowed_obs = []
     assert action_obs.shape[1] == rewards.shape[1]
     for j in range(action_obs.shape[0]):
-        for i in range(action_obs.shape[1] - window_size):
+        i = 0
+        while i < action_obs.shape[1] - window_size:
+        #for i in range(action_obs.shape[1] - window_size):
             windowed_action_obs.append(action_obs[j, i:i+window_size, :])
             windowed_rewards.append(rewards[j, i+window_size])
             windowed_obs.append(obs_all[j, i:i+window_size, :])
             windowed_action.append(action_all[j, i:i + window_size, :])
+            i+= window_size
     #print(action_all.shape, obs_all.shape)
     #print(np.asarray(windowed_action).shape, np.asarray(windowed_action).shape)
     return np.asarray(windowed_action_obs), np.asarray(windowed_rewards), np.asarray(windowed_action), np.asarray(windowed_obs)
@@ -43,12 +46,19 @@ def generate_data(env = gym.make('Pendulum-v0'), num_trajs = 1000, max_episode_l
                                                                num_trajs=num_trajs,
                                                                max_episode_length=max_episode_length)
     'Need to change this in the future for other environments'
-    reward_all += 17 #Make reward positive
+    #reward_all += 17 #Make reward positive
 
     action_obs = KDE.combine_obs_action(observation_all, action_all)
     x, new_rewards, action_all_new, obs_all_new = sliding_window(action_obs, reward_all, window_size, action_all, observation_all)
-    return action_all_new, obs_all_new, x, new_rewards
 
+
+    #print(obs_all_new.shape)
+    return action_all_new, obs_all_new,x, new_rewards
+def normalize(x):
+    ori_shape = x.shape
+    new_x = x.reshape(x.shape[0], -1)
+    new_x = (new_x - np.mean(new_x, axis = 0))/np.std(new_x, axis=0)
+    return new_x.reshape(ori_shape)
 
 def construct_dataset_PR(kde, x, new_rewards):
     # observation_all, reward_all, action_all = get_trajectories(random_agent, env=env,

Spectral_Learning.py
@@ -7,6 +7,7 @@ from Encoder_FC import Encoder_FC
 import pickle
 from Getting_Hankels import get_dataset
 import numpy as np
+from Getting_Hankels import get_all_kdes, get_data_generator
 import torch
 
 def convert_mpo_to_mps(mpo):
@@ -53,15 +54,15 @@ def test(dataset, cwfa, action_encoder, obs_encoder):
     tl.set_backend('numpy')
     pred = []
     for i in range(len(encoded_obs)):
-        #print(encoded_action[i].shape, encoded_obs[i].shape)
         pred.append(cwfa.predict(encoded_action[i], encoded_obs[i]))
     return np.asarray(pred).squeeze()
+
 if __name__ == '__main__':
     L = 2
     load_kde = True
     env_name = 'Pendulum-v0'
     lr = 0.001
-    epochs = 1000
+    epochs = 2000
 
     generator_params = {'batch_size': 512,
                         'shuffle': True,
@@ -70,14 +71,14 @@ if __name__ == '__main__':
                   'num_trajs': 500,
                   'max_episode_length': 10}
 
-    Hankel_params = {'rank': 5,
-                     'encoded_dim_action': 3,
-                     'encoded_dim_obs': 3,
+    Hankel_params = {'rank': 30,
+                     'encoded_dim_action': 5,
+                     'encoded_dim_obs': 5,
                      'hidden_units_action': [10],
                      'hidden_units_obs': [10],
                      'seed': 0,
                      'device': 'cpu',
-                     'rescale': True}
+                     'rescale': False}
     sampling_params_train = {'env': gym.make(env_name),
                              'num_trajs': 10000,
                              'max_episode_length': 10}
@@ -85,13 +86,43 @@ if __name__ == '__main__':
                             'num_trajs': 100,
                             'max_episode_length': 10}
     scheduler_params = {
-        'step_size': 50,
-        'gamma': 1
+        'step_size': 300,
+        'gamma': 0.5
     }
 
-    hankel_l, hankel_2l, hankel_2l1 = construct_all_hankels(L, load_kde, env_name, lr, epochs, generator_params,
-                                                            kde_params,
-                                                            sampling_params_train, sampling_params_vali, Hankel_params,
+    if not load_kde:
+        kde_l, kde_2l, kde_2l1 = get_all_kdes(kde_params, L)
+        pickle.dump(kde_l, open('kde_l' + env_name, 'wb'))
+        pickle.dump(kde_2l, open('kde_2l' + env_name, 'wb'))
+        pickle.dump(kde_2l1, open('kde_2l1' + env_name, 'wb'))
+    else:
+        kde_l = pickle.load(open('kde_l' + env_name, 'rb'))
+        kde_2l = pickle.load(open('kde_2l' + env_name, 'rb'))
+        kde_2l1 = pickle.load(open('kde_2l1' + env_name, 'rb'))
+
+    '''Getting generators for training and validation datasets'''
+    training_generators = {}
+    vali_generators = {}
+    training_dataset = {}
+    vali_dataset = {}
+
+    training_dataset['l'] = get_dataset(**sampling_params_train, kde=kde_l, window_size=L)
+    training_generators['l'] = get_data_generator(dataset=training_dataset['l'], **generator_params)
+    vali_dataset['l'] = get_dataset(**sampling_params_vali, kde=kde_l, window_size=L)
+    vali_generators['l'] = get_data_generator(dataset=vali_dataset['l'], **generator_params)
+
+    training_dataset['2l'] = get_dataset(**sampling_params_train, kde=kde_2l, window_size=2 * L)
+    training_generators['2l'] = get_data_generator(dataset=training_dataset['2l'], **generator_params)
+    vali_dataset['2l'] = get_dataset(**sampling_params_vali, kde=kde_2l, window_size=2 * L)
+    vali_generators['2l'] = get_data_generator(dataset=vali_dataset['2l'], **generator_params)
+
+    training_dataset['2l1'] = get_dataset(**sampling_params_train, kde=kde_2l1, window_size=2 * L + 1)
+    training_generators['2l1'] = get_data_generator(dataset=training_dataset['2l1'], **generator_params)
+    vali_dataset['2l1'] = get_dataset(**sampling_params_vali, kde=kde_2l1, window_size=2 * L + 1)
+    vali_generators['2l1'] = get_data_generator(dataset=vali_dataset['2l1'], **generator_params)
+
+    '''Learning Hankels'''
+    hankel_l, hankel_2l, hankel_2l1 = construct_all_hankels(L, lr, epochs, training_dataset, training_generators, vali_generators, Hankel_params,
                                                             scheduler_params)
     tl.set_backend('numpy')
     Hankel_l = extract_Hankel_and_encoder_from_NN(hankel_l)
@@ -101,7 +132,7 @@ if __name__ == '__main__':
     Hankels_mpo = [Hankel_l, Hankel_2l, Hankel_2l1]
     Hankels_mps = conver_all_hankel_to_mps(Hankels_mpo)
 
-    print(Hankels_mps[-1])
+    # print(Hankels_mps[-1])
 
     action_encoder = Encoder_FC(init_encoder=hankel_2l1.encoder_action)
     obs_encoder = Encoder_FC(init_encoder=hankel_2l1.encoder_obs)