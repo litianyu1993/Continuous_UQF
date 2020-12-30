39c7174561604306e8604bf3bad71fb439bc84d7
litianyu1993 <tianyu.li@mail.mcgill.ca>
2020-12-29 21:58:04 -0500

    clamped the actions to be within the action space
    clamped the actions to be within the action space. It seems that even don't change it, it won't affect anything, don't want to risk it.

--- sample_trajectories.py
+++ sample_trajectories.py
@@ -14,7 +14,10 @@ def get_trajectories(**option):
         'max_episode_length': 100,
         'uqf': None,
         'next_A': None,
-        'decoder': None
+        'decoder': None,
+        'range': (-1, 1),
+        'actions_mins': [-2],
+        'actions_maxs': [2]
     }
     option = {**option_default, **option}
     observation_all = []
@@ -26,23 +29,36 @@ def get_trajectories(**option):
         reward_epi = []
         action_epi = []
         for t in range(option['max_episode_length']):
-            #env.render()
+            #option['env'].render()
             if option['uqf'] is not None:
                 history = [action_epi, obs_epi]
                 from acting import UQF_agent
-                action = UQF_agent(option['uqf'], option['next_A'], option['decoder'], history, **option)
+                #print(option['uqf'])
+                agent_option = {'range': option['range']}
+                action = UQF_agent(uqf=option['uqf'], next_A=option['next_A'], decoder=option['decoder'], history = history, **agent_option)
+                #print(type(action), action.detach())
+                action = action.detach().numpy()
+                for i in range(len(action)):
+                    if action[i] > option['actions_maxs'][i]:
+                        action[i] = option['actions_maxs'][i]
+                    if action[i] < option['actions_mins'][i]:
+                        action[i] = option['actions_mins'][i]
             else:
                 action = option['agent'](option['env'], observation)
+
             #print(action)
             observation, reward, done, info = option['env'].step(action)
+            #print(action, reward)
             obs_epi.append(observation)
             action_epi.append(action)
             reward_epi.append(reward)
+            #print(reward)
             if done:
                 print("Episode finished after {} timesteps".format(t + 1))
                 break
         observation_all.append(obs_epi)
         reward_all.append(reward_epi)
+        #print(np.sum(np.asarray(reward_epi)))
         action_all.append(action_epi)
     option['env'].close()
     return np.asarray(observation_all), np.asarray(reward_all), np.asarray(action_all)
@@ -51,7 +67,7 @@ if __name__ == '__main__':
     option = {
         'agent': random_agent,
         'env': gym.make('Pendulum-v0'),
-        'num_trajs': 100,
+        'num_trajs': 1,
         'max_episode_length': 100
     }
     observation_all, reward_all, action_all = get_trajectories(**option)
