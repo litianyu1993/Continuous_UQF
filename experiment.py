470a8b169ded72c9357f68488ade2874b0ea9aa9
litianyu1993 <tianyu.li@mail.mcgill.ca>
2020-12-29 21:52:43 -0500

    Fixed the issue that using random policy for evaluation

--- experiment.py
+++ experiment.py
@@ -1,13 +1,14 @@
 from CWFA_AO import learn_CWFA
 import gym
 import torch
+import torch.nn.functional as F
 from acting import convert_wfa_to_uqf, get_decoder
 import numpy as np
 from sample_trajectories import get_trajectories
 import pickle
 if __name__ == '__main__':
     window_size = 5
-    load_cwfa = False
+    load_cwfa = True
     env = gym.make('Pendulum-v0')
     option_lists = {
         'kde_option': {
@@ -15,7 +16,7 @@ if __name__ == '__main__':
             'num_trajs': 100,
             'max_episode_length': 100,
             'window_size': window_size,
-            'load_kde': False
+            'load_kde': True
         },
         'train_gen_option': {
             'env': env,
@@ -24,7 +25,7 @@ if __name__ == '__main__':
             'window_size': window_size},
         'validate_gen_option': {
             'env': env,
-            'num_trajs': 1000,
+            'num_trajs': 100,
             'max_episode_length': 100,
             'window_size': window_size},
 
@@ -48,11 +49,13 @@ if __name__ == '__main__':
             'inner_activation': torch.nn.LeakyReLU(inplace=False)
         },
         'fit_option': {
-            'epochs': 1000,
+            'epochs': 350,
             'verbose': True,
             'lr': 0.001,
             'step_size': 100,
-            'gamma': 0.1
+            'gamma': 0.1,
+            'train_loss': F.mse_loss,
+            'vali_loss':F.mse_loss
         }
     }
 
@@ -66,26 +69,34 @@ if __name__ == '__main__':
         cwfa = learn_CWFA(**option_lists)
         pickle.dump(cwfa, f)
         f.close()
+    convert_option = {
+        'num_examples': 100000,
+        'range': [-1, 1]
+    }
 
-    uqf, next_A = convert_wfa_to_uqf(cwfa_ao=cwfa)
+    uqf, next_A = convert_wfa_to_uqf(cwfa_ao=cwfa, **convert_option)
     decoder_option = {
         'sample_size_train': 10000,
         'sample_size_vali': 1000,
         'lr': 0.001,
-        'epochs': 1000,
+        'epochs': 100,
         'gamma': 1,
         'step_size': 500,
-        'batch_size': 256
+        'batch_size': 256,
+        'encoder_range': [-1, 1]
     }
     action_decoder = get_decoder(uqf.action_encoder, **decoder_option)
 
     sampling_option = {
         'env': env,
-        'num_trajs': 10,
+        'num_trajs': 100,
         'max_episode_length': 100,
-        'uqf': None,
-        'next_A': None,
-        'decoder': None
+        'uqf': uqf,
+        'next_A': next_A,
+        'decoder': action_decoder,
+        'range': (-1, 1),
+        'actions_mins': [-2],
+        'actions_maxs': [2]
     }
     observation_all, reward_all, action_all = get_trajectories(**sampling_option)
     #print(reward_all)
